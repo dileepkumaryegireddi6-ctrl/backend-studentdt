184d7f3948efe6196c4c84aeea17ec2bc65de9b5
dileepkumaryegireddi6-ctrl <dileepkumaryegireddi6@gmail.com>
2026-05-19 21:58:18 +0530

    first commit

---
+++ model/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { type } = require("node:os");
+
+const userschema = new mongoose.Schema({
+    name:{
+        type:String,
+       
+    },
+    password:{
+        type:String,
+     
+    }, 
+    email:{
+        type:String,
+      
+    }
+})
+
+module.exports = mongoose.model("User",userschema);
